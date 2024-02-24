1fa1f0b99ddd73d025e8c764dc2226adee5e27b0
JasonChoyCode <86578475+JasonChoyCode@users.noreply.github.com>
2024-02-23 16:43:21 -0800

    navigation bar fixes

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import NavigationBar from './NavigationBar.js';
+import React from "react";
+import NavigationBar from "../components/NavigationBar.js";
+import "../css/About.css";
 
 export const About = () => {
-    // Dummy data for clubs (replace this with actual data)
-    const clubs = [
-        { name: 'President', description: 'Description of Club 1' },
-        { name: 'Vice-President', description: 'Description of Club 2' },
-        { name: 'Webmaster', description: 'Description of Club 3' },
-    ];
+  const clubs = [
+    { name: "President", description: "Description of Club 1" },
+    { name: "Vice-President", description: "Description of Club 2" },
+    { name: "Webmaster", description: "Description of Club 3" },
+  ];
 
-    return (
-        <div>
-            <header>
-                <h1>Learn more about ESUC and the team</h1>
-                <NavigationBar></NavigationBar>
-            </header>
-            <section>
-                <p>About ESUC</p>
-            </section>
-            <section>
-                <h2>Leadership Team</h2>
-                <ul>
-                    {clubs.map((club, index) => (
-                        <li key={index}>
-                            <strong>{club.name}</strong>: {club.description}
-                        </li>
-                    ))}
-                </ul>
-            </section>
-            <footer>
-                <p>&copy; 2023 My ESUC @ UCLA</p>
-            </footer>
-        </div>
-    );
-}
+  return (
+    <div className="about-container">
+      <div className="header-container">
+        <header>
+          <h1>Learn more about ESUC and the team</h1>
+        </header>
+        <NavigationBar />
+      </div>
+      <section>
+        <p>About ESUC</p>
+      </section>
+      <section>
+        <h2>Leadership Team</h2>
+        <ul>
+          {clubs.map((club, index) => (
+            <li key={index}>
+              <strong>{club.name}</strong>: {club.description}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <footer>
+        <p>&copy; 2023 My ESUC @ UCLA</p>
+      </footer>
+    </div>
+  );
+};
