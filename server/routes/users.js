449f28c6143ee5d1c7cdb66b1269c6951e894332
tsakshi01 <tsakshi01@gmail.com>
2024-04-23 00:31:58 -0700

    Registration/Login Complete

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const User = require('../models/userModel')
 
+const express = require('express')
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel')
 const router = express.Router()
 
 // GET all clubs
@@ -13,31 +14,46 @@ const router = express.Router()
 //     res.json({mssg: 'GET a single club'})
 // })
 // POST a new club
+
+const validPassword = (password) => { //checks that password conditions met
+    const valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+    return valid.test(password);
+}
+
 router.post('/auth/register', async (req, res) => {
-    const {firstName, lastName, email, password} = req.body
+    const {firstName, lastName, email, password} = req.body;
     
+    if (!validPassword(password)) {
+        return res.status(500).json({ error: "Invalid Passcode" });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt); //hashes the password
+    console.log(hash);
+
     try { 
-        const user = await User.create({firstName, lastName, email, password})
-        res.status(200).json(user)
+        const user = await User.create({firstName, lastName, email, password: hash});
+        res.status(200).json(user);
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({error: error.message});
     }
     //res.json({mssg: 'registered a user'})
 })
 
 router.post('/auth/login', async (req, res) => {
-    const {email, password} = req.body
+    const {email, password} = req.body;
     const user = await User.findOne({email: email});
-    console.log(user.password)
     if (user) {
-        if (user.password === password) {
-            res.json("Success")
+        console.log(user);
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (passwordMatch) {
+            res.status(200).json("Correct!");
         } else {
-            res.json("the password is incorrect")
+            res.status(400).json({error: error.message});
         }
     } else {
-        res.json("No record existed")
+        res.json("No record existed");
     }
 })
 
-module.exports = router
+module.exports = router;
