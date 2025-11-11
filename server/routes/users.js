db57ab6bbfb27c8d1468cbaeb142617cf698697f
Lian Elsa Linton <lianelsalinton@gmail.com>
2025-11-10 18:03:43 -0800

    update ci workflow

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,7 @@
-
-const express = require('express')
+const express = require('express');
 const bcrypt = require('bcrypt');
-const User = require('../models/userModel')
-const router = express.Router()
+const User = require('../models/userModel');
+const router = express.Router();
 
 // GET all clubs
 // router.get('/', (req, res) => {
@@ -15,45 +14,46 @@ const router = express.Router()
 // })
 // POST a new club
 
-const validPassword = (password) => { //checks that password conditions met
-    const valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
-    return valid.test(password);
-}
+const validPassword = (password) => {
+  //checks that password conditions met
+  const valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+  return valid.test(password);
+};
 
 router.post('/auth/register', async (req, res) => {
-    const {firstName, lastName, email, password} = req.body;
-    
-    if (!validPassword(password)) {
-        return res.status(500).json({ error: "Invalid Passcode" });
-    }
+  const { firstName, lastName, email, password } = req.body;
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt); //hashes the password
-    console.log(hash);
+  if (!validPassword(password)) {
+    return res.status(500).json({ error: 'Invalid Passcode' });
+  }
 
-    try { 
-        const user = await User.create({firstName, lastName, email, password: hash});
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(400).json({error: error.message});
-    }
-    //res.json({mssg: 'registered a user'})
-})
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt); //hashes the password
+  console.log(hash);
+
+  try {
+    const user = await User.create({ firstName, lastName, email, password: hash });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  //res.json({mssg: 'registered a user'})
+});
 
 router.post('/auth/login', async (req, res) => {
-    const {email, password} = req.body;
-    const user = await User.findOne({email: email});
-    if (user) {
-        console.log(user);
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (passwordMatch) {
-            res.status(200).json("Correct!");
-        } else {
-            res.status(400).json({error: error.message});
-        }
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (user) {
+    console.log(user);
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (passwordMatch) {
+      res.status(200).json('Correct!');
     } else {
-        res.json("No record existed");
+      res.status(400).json({ error: error.message });
     }
-})
+  } else {
+    res.json('No record existed');
+  }
+});
 
 module.exports = router;
