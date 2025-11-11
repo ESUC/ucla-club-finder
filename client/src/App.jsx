6e4b0f66e1925fc7d8fdc389ad78fe63d2f1d66b
Lian Elsa Linton <lianelsalinton@gmail.com>
2025-11-10 16:22:42 -0800

    added a new about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { Home } from "./pages/home";
-import { About } from "./pages/about";
-import { SavedClubs } from "./pages/saved-clubs";
+import { Login } from './pages/login';
+import { Register } from './pages/register';
+import { Home } from './pages/home';
+import { About } from './pages/about';
+import { SavedClubs } from './pages/saved-clubs';
 
 export const App = () => {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/auth/login" element={<Login /> } />
+          <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
-          <Route path="/home" element={<Home /> } />
-          <Route path="/about" element={<About /> } />
-          <Route path="/saved-clubs" element={<SavedClubs /> } />
-        </Routes>  
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/about34" element={<About />} />
+          <Route path="/saved-clubs" element={<SavedClubs />} />
+        </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
-
-
