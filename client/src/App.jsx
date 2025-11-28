c074abf6f25d19c7ab22d279d981bd45a5fa4d7e
Lian Elsa Linton <lianelsalinton@gmail.com>
2025-11-28 12:40:33 -0800

    fixed to pass linter checks + removed prettier checks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Login } from './pages/login';
 import { Register } from './pages/register';
