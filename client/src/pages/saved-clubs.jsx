94696237fd304f4457bdb41c7aaaad3ecbb57842
Lian Elsa Linton <lianelsalinton@gmail.com>
2025-11-28 12:35:29 -0800

    added edit profile page

--- client/src/pages/saved-clubs.jsx
+++ client/src/pages/saved-clubs.jsx
@@ -1,31 +1,50 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import Carousel from 'react-material-ui-carousel';
-
-import FlippableCard from '../components/FlippableCard';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import Footer from '../components/Footer/Footer';
+import ClubCarousel from '../components/ClubCarousel/ClubCarousel';
+import './SavedClubs.css';
 
 export const SavedClubs = () => {
-  const [currentIndex] = useState(0);
+  const [joinedClubs, setJoinedClubs] = useState([]);
+  const [savedClubs, setSavedClubs] = useState([]);
 
-  const Container = styled.div`
-    width: 20em;
-    margin: 0 auto;
-  `;
+  useEffect(() => {
+    // TODO: Fetch actual clubs from API
+    // For now, using placeholder data
+    const mockJoinedClubs = Array.from({ length: 20 }, (_, i) => ({
+      id: i + 1,
+      name: `Joined Club ${i + 1}`,
+      logo: null,
+    }));
+
+    const mockSavedClubs = Array.from({ length: 20 }, (_, i) => ({
+      id: i + 1,
+      name: `Club ${i + 1}`,
+      logo: null,
+    }));
+
+    setJoinedClubs(mockJoinedClubs);
+    setSavedClubs(mockSavedClubs);
+  }, []);
 
   return (
-    <>
-      <h1>Saved Clubs</h1>
+    <div className="saved-clubs-page">
       <NavigationBar />
-      <Container>
-        <Carousel index={currentIndex} autoPlay={false} navButtonsAlwaysVisible={true}>
-          {Array.from({ length: 9 }).map((_, index) => (
-            <FlippableCard key={index}>Card {index + 1}</FlippableCard>
-          ))}
-        </Carousel>
-      </Container>
+      <div className="saved-clubs-content">
+        <Link to="/edit-profile" className="saved-clubs-edit-button">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#043873" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+          </svg>
+          <span>Edit Profile</span>
+        </Link>
+        <ClubCarousel title="The Clubs I Am In" clubs={joinedClubs} />
+        <ClubCarousel title="Saved Clubs" clubs={savedClubs} />
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
+
+export default SavedClubs;
